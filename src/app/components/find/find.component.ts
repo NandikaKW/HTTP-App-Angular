f41b054dc870c7bab74e538fe21b0cdd115be642
NandikaKW <nandikakaweesha4005@gmail.com>
2026-02-21 00:17:13 +0530

    Refactor find  component to use PostService.

--- src/app/components/find/find.component.ts
+++ src/app/components/find/find.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Post, PostService } from '../services/post.service';
 
 @Component({
   selector: 'app-find',
@@ -8,20 +9,34 @@ import { HttpClient } from '@angular/common/http';
 })
 export class FindComponent implements OnInit {
   searchid: string = '';
-  list: any[] = [];
+  list: Post[] = [];
+
  
- loadData() {
-    this.http
-      .get<any[]>('https://jsonplaceholder.typicode.com/posts?id=' + this.searchid)
-      .subscribe(data => {
+loadData() {
+    const id = Number(this.searchid);  
+
+    if (!id) {
+      alert('Please enter a valid ID');
+      return;
+    }
+
+    
+    this.postService.find(id).subscribe(
+      data => {
         console.log(data);
-        this.list = data;
-      });
+        this.list = data ? [data] : [];  
+      },
+      error => {
+        console.error('Post not found', error);
+        this.list = [];
+      }
+    );
   }
   
 
 
-  constructor(private http: HttpClient) {}
+  constructor(private postService: PostService
+  ) {}
 
   ngOnInit(): void {
      
