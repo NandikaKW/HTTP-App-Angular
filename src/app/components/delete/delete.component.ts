17b92e74697cfdbc3e61233b23709abff4a67283
NandikaKW <nandikakaweesha4005@gmail.com>
2026-02-21 00:10:33 +0530

    Refactor delete component to use PostService.

--- src/app/components/delete/delete.component.ts
+++ src/app/components/delete/delete.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Post, PostService } from '../services/post.service';
 
 @Component({
   selector: 'app-delete',
@@ -9,41 +10,41 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class DeleteComponent implements OnInit {
 
-  
-    list: any[] = [];   
-  
-    constructor(private http: HttpClient,private _snackBar: MatSnackBar) {}
-  
-    ngOnInit(): void {
-      this.http
-        .get<any[]>('https://jsonplaceholder.typicode.com/posts')
-        .subscribe(data => {
-          console.log(data);
-          this.list = data;
-        });
-    }
-     // Function to delete a post by ID with confirm
+
+  list: Post[] = [];
+
+  constructor(
+    private postService: PostService,
+    private _snackBar: MatSnackBar
+  ) { }
+
+  ngOnInit(): void {
+    this.postService.findAll().subscribe(data => {
+      console.log(data);
+      this.list = data;
+    });
+  }
+  // Function to delete a post by ID with confirm
   delete(id: number) {
     // Show browser confirm dialog
     const confirmed = confirm('Are you sure you want to delete this post?');
-    
-    if (confirmed) {
-      // Send DELETE request
-      this.http.delete('https://jsonplaceholder.typicode.com/posts/' + id)
-        .subscribe(() => {
-          // Remove deleted post from list so table updates immediately
-          this.list = this.list.filter(item => item.id !== id);
 
-          // Show success snackbar notification
-          this._snackBar.open('Post Deleted Successfully!', 'Close', {
-            duration: 3000,
-            horizontalPosition: 'center',
-            verticalPosition: 'top'
-          });
+    if (confirmed) {
+      this.postService.delete(id).subscribe(() => {
+        // Remove deleted post from list immediately
+        this.list = this.list.filter(item => item.id !== id);
+
+        // Show success notification
+        this._snackBar.open('Post Deleted Successfully!', 'Close', {
+          duration: 3000,
+          horizontalPosition: 'center',
+          verticalPosition: 'top'
         });
+      });
     }
   }
-
-    
-
 }
+
+
+
+
