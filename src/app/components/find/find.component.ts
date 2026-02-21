d4dfe69dd6422d3b27a57de62d21bbceeaeec58e
NandikaKW <nandikakaweesha4005@gmail.com>
2026-02-21 13:15:19 +0530

    find component ui updated.

--- src/app/components/find/find.component.ts
+++ src/app/components/find/find.component.ts
@@ -10,36 +10,37 @@ import { Post, PostService } from '../services/post.service';
 export class FindComponent implements OnInit {
   searchid: string = '';
   list: Post[] = [];
+  isLoading = false;
+  searchPerformed = false;
 
- 
-loadData() {
-    const id = Number(this.searchid);  
+  loadData() {
+    const id = Number(this.searchid);
 
     if (!id) {
       alert('Please enter a valid ID');
       return;
     }
 
-    
+    this.isLoading = true;
+    this.searchPerformed = true;
+
     this.postService.find(id).subscribe(
       data => {
         console.log(data);
-        this.list = data ? [data] : [];  
+        this.list = data ? [data] : [];
+        this.isLoading = false;
       },
       error => {
         console.error('Post not found', error);
         this.list = [];
+        this.isLoading = false;
       }
     );
   }
-  
 
-
-  constructor(private postService: PostService
-  ) {}
+  constructor(private postService: PostService) {}
 
   ngOnInit(): void {
-     
+    this.searchPerformed = false;
   }
-
 }
